715b15e2e8811db734005f0aba43e2e5d2386c17
Tomas Guardo <guardotomas02@gmail.com>
2021-03-04 17:42:46 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const server = express();
+const port = 3000;
+
+server.get("/", (req, res) => {
+    res.send("hello")
+})
+
+server.listen(port, () => {
+    console.log("listening to port: " + port);
+})
