4b5dad241519576bb5abb8010fa3ca7832b1b5f6
Tomas Guardo <guardotomas02@gmail.com>
2021-03-05 00:29:05 -0300

    started header
    finished general styling of header and navigation bar

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path")
 
 const server = express();
 const port = 3000;
 
+server.use(express.static(__dirname + '/public'));
+server.set("views", path.join(__dirname + "/public"))
+server.set("view engine", "ejs");
+
 server.get("/", (req, res) => {
-    res.send("hello")
+    res.render("landing", {logoSvg: __dirname + "/public/media/logo-e.svg"});
 })
 
 server.listen(port, () => {
