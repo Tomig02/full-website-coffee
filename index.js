e94003ae01f715fb50eba16e406a985b2c827a5d
Tomas Guardo <guardotomas02@gmail.com>
2021-03-05 20:51:44 -0300

    finished landing page
    finished adding and styling the landing page and footer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { url } = require("inspector");
 const path = require("path")
 
 const server = express();
@@ -7,9 +8,9 @@ const port = 3000;
 server.use(express.static(__dirname + '/public'));
 server.set("views", path.join(__dirname + "/public"))
 server.set("view engine", "ejs");
-
+ 
 server.get("/", (req, res) => {
-    res.render("landing", {logoSvg: __dirname + "/public/media/logo-e.svg"});
+    res.render("mainSite", {page: __dirname + "/public/landingPage.ejs"});
 })
 
 server.listen(port, () => {
