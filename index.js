1cd09b60e59b7ead5f7767e99a9a19c39959409e
Tomas Guardo <guardotomas02@gmail.com>
2021-03-07 00:04:27 -0300

    new pages and changed bootstrap to local
    Changed the usage of bootstrap from remote with link tags to local module.
    Added coffee products and about us pages

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const { url } = require("inspector");
-const path = require("path")
+const path = require("path");
 
 const server = express();
 const port = 3000;
 
 server.use(express.static(__dirname + '/public'));
-server.set("views", path.join(__dirname + "/public"))
+server.set("views", path.join(__dirname + "/public"));
 server.set("view engine", "ejs");
  
 server.get("/", (req, res) => {
     res.render("mainSite", {page: __dirname + "/public/landingPage.ejs"});
-})
+});
+
+server.get("/cafe", (req, res) => {
+    res.render("mainSite", {page: __dirname + "/public/coffePage.ejs"})
+});
+
+server.get("/conocenos", (req, res) => {
+    res.render("mainSite", {page: __dirname + "/public/aboutUs.ejs"})
+});
+
 
 server.listen(port, () => {
     console.log("listening to port: " + port);
-})
+});
